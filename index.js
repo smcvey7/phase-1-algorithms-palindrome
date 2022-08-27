d662b53fae3d561aaa69d82a696d552ddb05c5c3
Steven McVey <smcvey1210@gmail.com>
2022-08-27 14:11:20 -0400

    commit tree 5746602ede168d9a3a8c00810922528e40bef7ee

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let palindromeTest = false
+  for (i=0; i<word.length; i++){
+    if (word[i]===word[word.length-(i+1)]){
+      palindromeTest = true;
+    }else{
+      return false
+    }
+  }
+  return palindromeTest
 }
 
 /* 
   Add your pseudocode here
+  Store test-pass for each letter in a variable
+  For each letter position in order
+    Check if letter is the same as corresponding letter from end
+      if same--> test variable is true
+      if false function ends returning false
+  If all letters were the same, test variable is returned true
 */
 
 /*
   Add written explanation of your solution here
+  Initially tried to use word[-(i+1)] to compare letters counted from the end
+  but apparently negative indices don't work in JS? I think they can be used in some
+  situations, but maybe that was something that worked in Python that I just carried
+  over... So I had to use the word length and subtract one each time.
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,10 +35,16 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome('hannah'));
+
+  console.log('Expecting: false');
+  console.log("=>", isPalindrome('baobab'));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
+
+isPalindrome('racecar')
